a4deecf598ec51f52e6e085061c37c06730f8133
NHAT\rimng <nguyentrongnhat111999@gmail.com>
2026-06-03 16:03:19 +0700

    BE - create dto for UserDetail Entity

--- backend/src/main/java/com/nguyen_trong_nhat/you_and_i/features/user/controller/UserController.java
+++ backend/src/main/java/com/nguyen_trong_nhat/you_and_i/features/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.nguyen_trong_nhat.you_and_i.features.user.controller;
 
-import com.nguyen_trong_nhat.you_and_i.common.security.service.impl.UserDetailsServiceImpl;
 import com.nguyen_trong_nhat.you_and_i.features.user.entity.MyUserDetail;
 import com.nguyen_trong_nhat.you_and_i.features.user.service.UserService;
 import lombok.AllArgsConstructor;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
