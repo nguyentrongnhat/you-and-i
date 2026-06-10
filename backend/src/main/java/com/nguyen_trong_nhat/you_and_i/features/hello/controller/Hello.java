814234d44e25ec5e2873d49f92c16671327c0c06
NHAT\rimng <nguyentrongnhat111999@gmail.com>
2026-06-10 17:39:00 +0700

    BE - implement API for test env

--- backend/src/main/java/com/nguyen_trong_nhat/you_and_i/features/hello/controller/Hello.java
+++ backend/src/main/java/com/nguyen_trong_nhat/you_and_i/features/hello/controller/Hello.java
@@ -1,5 +1,6 @@
 package com.nguyen_trong_nhat.you_and_i.features.hello.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api")
 public class Hello {
+    @Value("${api.env.test}")
+    private String envTest;
+
     @GetMapping("")
     public String helloWorld() {
-        return "Hello world";
+        return "Hello world: " + envTest;
     }
 }
