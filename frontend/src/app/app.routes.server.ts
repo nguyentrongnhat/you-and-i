8fb08a8ef02f90e66d275bec98ecca6f0e09844f
NHAT\rimng <nguyentrongnhat111999@gmail.com>
2026-06-10 14:43:46 +0700

    FE - config route to build angular

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,6 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { ROUTE_PATHS } from './core/constants/route-paths';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: ROUTE_PATHS.USER.children.DETAIL.fullPath,
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
