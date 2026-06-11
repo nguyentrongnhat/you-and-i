ecb3c35c48f3e00def81f3982d3a55d17491ceb7
NHAT\rimng <nguyentrongnhat111999@gmail.com>
2026-06-11 16:41:28 +0700

    FE - change base uri to deploy

--- frontend/src/enviroments/environments.ts
+++ frontend/src/enviroments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api'
+  apiBaseUrl: 'https://jar-of-messages-api.azurewebsites.net/api' //'http://localhost:8080/api'
 };
