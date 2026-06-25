9d5c4a4e27b25e5bf39c3fa7c49f11d782e9c91b
APAC\gbb1hc <gbb1hc@bosch.com>
2026-06-25 17:03:48 +0700

    FE - refactor code

--- frontend/src/app/features/games/find-number-game/components/game-ranking/game-ranking.ts
+++ frontend/src/app/features/games/find-number-game/components/game-ranking/game-ranking.ts
@@ -1,13 +1,13 @@
-import { DatePipe, SlicePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { Component, computed, inject } from '@angular/core';
 import { TableModule } from 'primeng/table';
-import { FindNumberGameService } from '../../services/findnumber.service';
 import { FindNumberGameBestRecordDTO } from '../../../../../core/interfaces/find-number-game.dto';
 import { UserService } from '../../../../user-management/services/user.service';
+import { FindNumberGameService } from '../../services/findnumber.service';
 
 @Component({
   selector: 'app-game-ranking',
-  imports: [DatePipe, SlicePipe, TableModule],
+  imports: [DatePipe, TableModule],
   templateUrl: './game-ranking.html',
   styleUrl: './game-ranking.scss',
 })
